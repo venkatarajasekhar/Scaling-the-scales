34a2ccfd010eda49ab6c120099654c507dd507e3
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2016-03-08 20:14:09 +0530

    user-inverse-offset-scaled.cpp

--- DatasetGenerator/user/user-inverse-offset-scaled.cpp
+++ DatasetGenerator/user/user-inverse-offset-scaled.cpp
@@ -6,10 +6,17 @@
  */
 int UserInverseOffsetScaled::rating(double const itemAvg) {
    double rating = m_factory->rating(itemAvg);
-   
+   try{
    rating = m_min + (rating-1)/(m_factory->scaleSize-1)*(m_max-m_min);
-   
+   }catch(exception& e) //Takes a reference to an 'exception' object
+    {
+        cout << "Stack Over Flow " << e.what() << endl;
+    }
+   try{
    rating = std::max(1, std::min((int)m_factory->scaleSize, (int)round(rating)));
-   
+   }catch(exception& e) //Takes a reference to an 'exception' object
+    {
+        cout << "Stack Over Flow " << e.what() << endl;
+    }
    return (int)rating;
 } // rating
